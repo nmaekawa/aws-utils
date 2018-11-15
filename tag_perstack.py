f647b7d63313377fa0ceab65ecb8502e96fc5646
nmaekawa <nmaekawa@g.harvard.edu>
2018-11-15 12:08:55 -0500

    add some checks for stack name pattern
    - beware that we are checking a pattern in the name and that can
      filter in more than one might expect; e.g. "hximg-devo-naomi" will
      filter in all devo stacks naomi cared to launch and not delete...

--- tag_perstack.py
+++ tag_perstack.py
@@ -7,19 +7,20 @@ import boto3
 # define the connection region
 ec2 = boto3.resource('ec2', region_name='us-west-2')
 
-def usage():
-    print('missing comma separated list of instance_ids')
-    exit(1)
 
+def valid_stack_name(stack_name):
+    """minimal check for hx stack-name pattern.
+    
+    needed because we filter per PATTERN and if you give a stack_name
+    like 'devo', all devo stack tags will be overwritten!
 
-def copy_tags2vol(ec2_base, tags2copy):
-
-    for instance in ec2_base:
-        for t in instance.tags:
-            if t['Key'] in tags2copy:
-                for vol in instance.volumes.all():
-                    add_tag2vol(t, vol, replace=replace_tag)
-
+    the pattern for hx stack-names is <project>-<cluster>-<some_uniq_id>,
+    so counting the number of '-' dashes in stack-name.
+    """
+    if stack_name.count('-') >= 2 and not stack_name.endswith('-'):
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
 
@@ -32,25 +33,37 @@ if __name__ == '__main__':
         required=False,
         default='project,cluster,service',
         help='comma separated list of tags to be copied')
+    parser.add_argument(
+        '--quiet',
+        action='store_true',
+        help='does not print any info')
 
     # parse args
     args = parser.parse_args()
-    stack = args.stackname
     instance_tags = args.tags.split(',')
+    stack = args.stackname.strip()
+    if not valid_stack_name(stack):
+        if not args.quiet:
+            print('invalid stack name({})'.format(args.stackname))
+        exit(1)
 
-    #print('stack name is {}'.format(args.stackname))
-    #print('list of tags is {}'.format(instance_tags))
-    #exit()
-
-    filters = [{'Name': 'tag-key', 'Values':['aws:cloudformation:stack-id']}]
+    filters = [{'Name': 'tag-key', 'Values':['aws:cloudformation:stack-name']}]
     instances = ec2.instances.filter(Filters=filters)
     stack_instances = []
-    print('instances {}'.format(type(instances)))
+    stack_names = []
     for i in instances:
         for tag in i.tags:
-            if tag['Key'] == 'aws:cloudformation:stack-id':
-                if stack in tag['Value']:
+            if tag['Key'] == 'aws:cloudformation:stack-name':
+                if tag['Value'].startswith(stack):
                     stack_instances.append(i)
+                    stack_names.append(tag['Value'])
+
+    if not args.quiet:
+        if not stack_instances:
+            print('no instances found for stack name ({})'.format(stack))
+            exit()
+        else:
+            print('found instances in stack(s): {}'.format(set(stack_names)))
 
     for instance in stack_instances:
         # adding stack name because nested cf-stacks get their own stack-name
